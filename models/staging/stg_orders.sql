49b229ca15a898182cd2ebc47c330833ad69fcfe
anergha.mohan@ocean.ibm.com <anergha.mohan@ocean.ibm.com>
2023-01-19 07:21:08 +0000

    Commiting stage orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,3 +1,18 @@
 select *, OrderSellingPrice - OrderCostPrice
 as orderProfit 
 from {{ ref('raw_orders') }}
+
+select 
+--from orders
+o.OrderID,o.OrderDate,o.ShipDate,o.ShipMode,
+o.OrderSellingPrice - o.OrderCostPrice as orderProfit ,
+--from customer
+c.CustomerID,c.CustomerName,c.country,
+--from product
+p.Category , p.ProductName , p.Subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.CustomerID = c.CustomerID
+left join {{ ref('raw_product') }} as p
+on o.productId = p.productId
+
